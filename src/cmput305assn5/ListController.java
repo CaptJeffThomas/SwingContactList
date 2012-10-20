994e3ecaf002f0ef8961291c44091610b743a22f
Jeff <jeff@Primer>
2012-10-20 17:22:18 -0600

    Runs smooth.  Need to implement the New Contact functionality and, time permitting, sorting the contacts alphabetically.

--- src/cmput305assn5/ListController.java
+++ src/cmput305assn5/ListController.java
@@ -15,34 +15,78 @@ public class ListController {
     
     private List<Contact> contactList;
     private ListGUI view;
+    private JTable contactTable;
+    private DefaultTableModel tableModel;
+    
+    
     
     public ListController(ListGUI gui){
         
         contactList = new ArrayList();
-        this.view = gui;
-   
-        this.view.addNewButtonListener(new NewListener());
-        this.view.addDeleteButtonListener(new DeleteListener());
+        view = gui;
+        contactTable = this.view.getCourseTable();
+        
+        view.addNewButtonListener(new NewListener());
+        view.addDeleteButtonListener(new DeleteListener());
+        
+        initTableModel();
+        initListSelectionModel();
         
         
     }
     
+      //creates our local table model
+     private void initTableModel() {
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("Name");
+        tableModel.addColumn("Email");
+        contactTable.setModel(tableModel);        
+    }
+     
+      private void initListSelectionModel() {
+        ListSelectionModel lsm = contactTable.getSelectionModel();
+        lsm.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        lsm.addListSelectionListener(new CourseTableListener());
+    }
     
     //pops up New Contact menu
     private class NewListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-           
+           Object[] yup = {"big dog", "jericho"};
+           contactList.add(new Contact("big dog", "williams", "jericho@internets.ca", "780-5557", "heisenberg work phone"));
+           tableModel.addRow(yup);
         }
     }
     
-    //removes selected contact from List
+    //removes selected contact from List and clears contact details
     private class DeleteListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-           
+            ListSelectionModel lsm = contactTable.getSelectionModel();
+            if (!lsm.isSelectionEmpty()) {
+                contactList.remove(lsm.getMinSelectionIndex());
+                tableModel.removeRow(lsm.getMinSelectionIndex());
+                view.setDetailedText("");
+                view.setEnabledDeleteButton(false);
+            }
+        }
+    }
+    
+    //when contact is selected show detailed info and enable delete funtionality
+    private class CourseTableListener implements ListSelectionListener {
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            ListSelectionModel lsm = (ListSelectionModel) e.getSource();
+            if (!e.getValueIsAdjusting()) {
+                //if we have selected a contact, enable delete and display details
+                if(!lsm.isSelectionEmpty()){
+                    view.setEnabledDeleteButton(true);
+                    detailedDisplay(contactList.get(e.getFirstIndex()));
+                }
+            }
         }
     }
     
